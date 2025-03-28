461e3151301ebc0fa04a4344d333e7b61c5b7592
CarlosEstra <114842445+CarlosEstra@users.noreply.github.com>
2025-03-28 12:25:05 -0700

    conexionBD

---
+++ rocio/ConexionBD.php
@@ -0,0 +1,30 @@
+<?php
+// Configuración de la base de datos
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '140223');
+define('DB_NAME', 'servinowbd');
+
+class Conexion {
+    private $conexion;
+
+    public function __construct() {
+        $this->conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        
+        if ($this->conexion->connect_error) {
+            die("Error de conexión: " . $this->conexion->connect_error);
+        } else {
+            echo "¡Conexión exitosa a la base de datos!"; // Mensaje de éxito
+        }
+        
+        $this->conexion->set_charset("utf8");
+    }
+
+    public function getConexion() {
+        return $this->conexion;
+    }
+}
+
+// Prueba instantánea
+$test = new Conexion(); // Esto mostrará el mensaje si la conexión es exitosa
+?>
---
+++ rocio/login.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+    require_once 'conexionBD.php';
+     
+    //recibdir datos del formulario
+    $telefono=$_post['Telefono'];
+    $password=$_post['Password'];
+
+    //Conexion a la BD
+    $db = new Conexion();
+    $conexion = $db->getConexion();
+
+
+    //buscar al usuario por telefono
+    $sql = "Select id, nombre, password, rol From Usuarios WHERE telefono = ? ";
+    $stmt =  $conexion->prepare($sql);
+    $stmt =  $execute();
+    $resultado = $stmt->get_result();
+
+
+    //verificar crendenciales
+      if ($resultado->num_rows === 1){
+        $usuario = $resultado->fetch_assoc();
+        if(password_verify($password,$usuario['password'])){
+        
+
+    //iniciar sesion
+    $_SESSION['usuario'] = [
+        'id'=>$usuario['id'],
+        'nombre'=>$usuario['nombre'],
+        'rol'=>$usurario['rol'],
+        'Telefono'=>$telefono
+    ];
+    header("Location:/index.php"); //redirigir al inicio
+    exit;
+
+        }
+      }
+      
+      //Si hay fallo regresa con error
+
+      header("Location:/Login.html?error=1");
+
+
+?>
